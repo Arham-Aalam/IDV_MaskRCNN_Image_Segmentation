5f356b350d4e8a1d6bb76fd80664c1346a4d9b7e
arham <arham.aalam18@gmail.com>
2019-05-16 12:26:07 +0530

    Fix input size

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1848,7 +1848,7 @@ class MaskRCNN():
 
         # Inputs
         input_image = KL.Input(
-            shape=[None, None, 3], name="input_image")
+            shape=[None, None, 1], name="input_image")
         input_image_meta = KL.Input(shape=[config.IMAGE_META_SIZE],
                                     name="input_image_meta")
         if mode == "training":
