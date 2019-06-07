f4cc023117cd6db713bb1e90eed272a5fe864dd9
arham <arham.aalam18@gmail.com>
2019-06-07 20:49:44 +0530

    remove data augmentation

--- samples/idvscript/main.py
+++ samples/idvscript/main.py
@@ -242,7 +242,7 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=50,
-                augmentation=augmentation,
+                #augmentation=augmentation,
                 layers='all')
 
 
